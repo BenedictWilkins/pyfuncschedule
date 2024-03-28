4bbace7a7abc256faa4a1fdfc5c93bb7c8580690
Benedict Wilkins <benrjw@gmail.com>
2024-03-28 12:27:45 +0000

    tests

--- pyfuncschedule/parser.py
+++ pyfuncschedule/parser.py
@@ -23,12 +23,83 @@ class VFuncCall:
     def __call__(self):
         return self._func(*self._resolve_arguments())
 
+    def __str__(self):
+        return f"{self._name}({','.join(str(arg) for arg in self._arguments)})"
+
+    def __repr__(self):
+        return str(self)
+
 
 class VSchedule:
 
     def __init__(self, intervals, repeat):
         self._intervals = intervals
         self._repeat = repeat
+
+    def __str__(self):
+        return f"[{','.join(str(arg) for arg in self._intervals)}]:{self._repeat}"
+
+    def __repr__(self):
+        return str(self)
+
+    def __iter__(self):
+        for _ in range(self._repeat):
+            for interval in self._intervals:
+                if isinstance(interval, VSchedule):
+                    yield from interval
+                elif isinstance(interval, VFuncCall):
+                    yield float(interval())
+                else:
+                    yield float(interval)
+
+
+class VActionSchedule:
+
+    def __init__(self, action: VFuncCall, schedule: VSchedule):
+        self._action = action
+        self._schedule = schedule
+
+    def __iter__(self):
+        _iter = iter(self._schedule)
+        yield (next(_iter), None)  # get initial interval to schedule next call
+        for interval in _iter:
+            yield (interval, self._action())
+        yield (None, self._action())  # final call to action
+
+    def __str__(self):
+        return f"{self._action}@{self._schedule}"
+
+    def __repr__(self):
+        return str(self)
+
+    async def __aiter__(self):
+        import asyncio
+
+        try:
+            _iter = iter(self)
+            # initial wait
+            wait_time, _ = next(_iter)  # the first action is always None
+            await asyncio.sleep(wait_time)
+
+            while True:
+                wait_time, action = next(_iter)
+                if wait_time is None:
+                    yield action
+                    raise StopIteration
+                future = asyncio.sleep(wait_time)
+                yield action
+                await future
+        except StopIteration:
+            pass
+
+    async def asyncio_runner(self):
+        """
+        This is a simple schedule runner that uses `asyncio`.
+        Given the nature of asyncio there is no guarentee that actions will execute on time,
+        especially if there are long-running tasks in the asycnio event loop.
+        """
+        async for _ in self:
+            pass
 
 
 class ScheduleParser:
@@ -37,32 +108,40 @@ class ScheduleParser:
         self._allowed_functions = {}
         self._allowed_actions = {}
 
-    def add_function(self, name: str, func: Callable):
+    def register_action(self, action: Callable, name: str = None):
+        """Add an action to the list of allowed actions."""
+        if name is None:
+            name = action.__name__
+        self._allowed_actions[name] = action
+
+    def register_function(self, func: Callable, name: str = None):
         """Add a function to the list of allowed functions."""
+        if name is None:
+            name = func.__name__
         self._allowed_functions[name] = func
 
     def parse(self, schedule: str):
-        result = self._parser.parseString(
+        return self._parser.parseString(
             schedule,
             parse_all=True,
         )[0].as_list()
 
-        resolved = self._resolve_iter(result)
+    def resolve(self, parse_result):
+        return list(self._resolve_iter(parse_result))
 
     def _resolve_iter(self, parse_result):
         for action, schedule in parse_result:
-            print(action, schedule)
             raction = ScheduleParser.resolve_action(
                 action, self._allowed_actions, self._allowed_functions
             )
             rschedule = ScheduleParser.resolve_schedule(
                 schedule, self._allowed_functions
             )
-            yield raction, rschedule
+            yield VActionSchedule(raction, rschedule)
 
     @staticmethod
     def resolve_schedule(schedule, valid_funcs):
-        repeat = schedule.repeat
+        repeat = ScheduleParser.resolve_repeat(schedule.repeat, valid_funcs)
         intervals = list(
             ScheduleParser.resolve_arguments(schedule.schedule, valid_funcs)
         )
@@ -83,17 +162,26 @@ class ScheduleParser:
         return VFuncCall(name, args, func)
 
     @staticmethod
+    def resolve_repeat(repeat, valid_funcs):
+        if isinstance(repeat, FuncCall):
+            return ScheduleParser.resolve_func(repeat, valid_funcs)
+        else:
+            return repeat
+
+    @staticmethod
     def resolve_arguments(arguments, valid_funcs):
         for arg in arguments:
-            if isinstance(FuncCall, arg):
+            if isinstance(arg, FuncCall):
                 yield ScheduleParser.resolve_func(arg, valid_funcs)
+            elif isinstance(arg, Schedule):
+                yield ScheduleParser.resolve_schedule(arg, valid_funcs)
             else:
                 yield arg
 
     @staticmethod
     def validate_func(name, args, valid_funcs):
         if name not in valid_funcs:
-            raise ValueError(f"Unknown function: {name}")
+            raise ValueError(f"Unregistered function: {name}")
         func = valid_funcs[name]
 
         sig = inspect.signature(func)
---
+++ test/test_schedule_iter.py
@@ -0,0 +1,95 @@
+import unittest
+from pyfuncschedule import ScheduleParser
+
+
+class TestActionScheduleIter(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.state = 0
+
+    def test_iter(self):
+        parser = ScheduleParser()
+
+        def foo1():
+            self.state += 1
+
+        parser.register_action(foo1)
+        schedule = """foo1()@[1,2]"""
+        result = parser.resolve(parser.parse(schedule))[0]
+        intervals = [interval for interval, action in result]
+        self.assertListEqual(intervals, [1.0, 2.0, None])
+        self.assertEqual(self.state, 2)
+
+    def test_repeated_iter(self):
+        parser = ScheduleParser()
+
+        def foo1(arg):
+            self.state += arg
+
+        parser.register_action(foo1)
+        schedule = """foo1(2)@[1,2]:2"""
+        result = parser.resolve(parser.parse(schedule))[0]
+        intervals = [interval for interval, action in result]
+        self.assertListEqual(intervals, [1.0, 2.0, 1.0, 2.0, None])
+        self.assertEqual(self.state, 8)
+
+    def test_nested_iter(self):
+        parser = ScheduleParser()
+
+        def foo1(arg):
+            self.state += arg
+
+        parser.register_action(foo1)
+        schedule = """foo1(2)@[1,[2]:2]:2"""
+        result = parser.resolve(parser.parse(schedule))[0]
+        intervals = [interval for interval, action in result]
+        self.assertListEqual(intervals, [1.0, 2.0, 2.0, 1.0, 2.0, 2.0, None])
+        self.assertEqual(self.state, 12)
+
+    def test_deep_nested_iter(self):
+        from ast import literal_eval
+
+        parser = ScheduleParser()
+
+        def foo1(arg):
+            self.state += arg
+
+        parser.register_action(foo1)
+        schedule = """foo1(2)@[1,[1,[2]:2]:2]:2"""
+        result = parser.resolve(parser.parse(schedule))[0]
+        intervals = [interval for interval, action in result]
+        self.assertListEqual(
+            intervals,
+            literal_eval(
+                "[1.0, 1.0, 2.0, 2.0, 1.0, 2.0, 2.0, 1.0, 1.0, 2.0, 2.0, 1.0, 2.0, 2.0, None]"
+            ),
+        )
+        self.assertEqual(self.state, 2 * (len(intervals) - 1))
+
+    def test_function_call(self):
+        parser = ScheduleParser()
+
+        def baz():
+            return self.state
+
+        def foo1(arg):
+            self.state += arg
+
+        parser.register_function(baz)
+        parser.register_action(foo1)
+        schedule = """foo1(2)@[baz()]:3"""
+        result = parser.parse(schedule)
+        result = parser.resolve(result)[0]
+
+        intervals = [interval for interval, action in result]
+        # 1st call to baz happens without a call to foo1
+        # 2nd call to baz happens BEFORE the first call to foo1
+        # 3rd call to baz happens AFTER the first call but BEFORE the second call to foo1
+        # RULE: functions calls in intervals always happen BEFORE the action call
+        self.assertListEqual(intervals, [0.0, 0.0, 2.0, None])
+        self.assertEqual(self.state, 6)
+
+
+if __name__ == "__main__":
+    unittest.main()
