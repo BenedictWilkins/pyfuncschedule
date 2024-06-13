b320753baa0661930ac57d7919fa88cc7f110677
Benedict Wilkins <benrjw@gmail.com>
2024-06-13 10:59:28 +0100

    setup updates

--- setup.py
+++ setup.py
@@ -5,10 +5,17 @@ setup(
     version="0.1",
     author="Benedict Wilkins",
     author_email="benrjw@gmail.com",
-    description="",
+    description="A simple schedule parser/runner with a convienient syntax.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=find_packages(),
+    url="https://github.com/BenedictWilkins/pyfuncschedule"
     install_requires=["pyparsing", "aiostream"],
     python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
